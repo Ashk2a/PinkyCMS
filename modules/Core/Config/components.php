f5fc2818476b45e5b1eb671135559cf5be990319
Ashk <timed2a@hotmail.fr>
2021-04-10 22:49:23 +0200

    [REMOVE] Components keep only form for the moment

--- modules/Core/Config/components.php
+++ modules/Core/Config/components.php
@@ -4,10 +4,6 @@ use Modules\Core\Components;
 
 return [
     'items' => [
-        // GLOBAL
-        Components\Card::class,
-        Components\Headline::class,
-        Components\Link::class,
         // FORM
         Components\Form\Form::class,
         Components\Form\FormCheckbox::class,
